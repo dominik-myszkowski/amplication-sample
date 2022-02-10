17c081600fed8828413f3b6e9cf0d47eaf40d5c0
dominik-myszkowski <40465966+dominik-myszkowski@users.noreply.github.com>
2022-02-10 15:44:29 +0100

    amplication-build-ckzh3dhuz56399701s65oi1paa3

---
+++ admin-ui/src/user/roles.ts
@@ -0,0 +1,14 @@
+export const ROLES = [
+  {
+    name: "user",
+    displayName: "User",
+  },
+  {
+    name: "manager",
+    displayName: "manager",
+  },
+  {
+    name: "operator",
+    displayName: "operator",
+  },
+];
